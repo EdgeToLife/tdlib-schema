879e0d35d7c0dd15cd1e3c4dfe04429ea39ff65c
Ivan Zhukov <ziv@servers.com>
2025-02-16 22:51:22 +0200

    Corrected EmojiStatusType

--- lib/tdlib/types/emoji_status_type/custom_emoji.rb
+++ lib/tdlib/types/emoji_status_type/custom_emoji.rb
@@ -1,5 +1,8 @@
 module TD::Types
+  # The emoji status represented by a custom emoji.
+  #
+  # @attr custom_emoji_id [Integer] Identifier of the custom emoji in {TD::Types::StickerFormat::Tgs} format.
   class EmojiStatusType::CustomEmoji < EmojiStatusType
-    attribute :custom_emoji_id, TD::Types::Coercible::Integer.optional.default(nil)
+    attribute :custom_emoji_id, TD::Types::Coercible::Integer
   end
 end
